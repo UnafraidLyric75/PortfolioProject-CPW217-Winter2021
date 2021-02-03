28c87eb9ebd4ce006f24d80d74854808ad43ed11
John Uthus <john.a.uthus.jr1@gmail.com>
2021-02-02 19:25:26 -0800

    Add project files.

---
+++ NationalParksAcrossAmerica/Models/ParkModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NationalParksAcrossAmerica.Models
+{
+    public class ParkModel
+    {
+        [Key]
+        public int ParkId { get; set; }
+
+        /// <summary>
+        /// Name of the park ex. YellowStone National Park, 
+        /// Redwood National Forest
+        /// Dashpoint State Park
+        /// </summary>
+        public string ParkName { get; set; }
+
+        /// <summary>
+        /// The park type is if it is a momument, State, Natioanl, Forest, etc.
+        /// </summary>
+        public string ParkType { get; set; }
+
+        /// <summary>
+        /// Which country the park is located in
+        /// </summary>
+        public string ParkNation { get; set; }
+
+        /// <summary>
+        /// where the park is in a state, ie Quebec, New York, Saxony, Wessex
+        /// </summary>
+        public string ParkState { get; set; }
+    }
+}
